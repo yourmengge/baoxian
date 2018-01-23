40c53343bda3e488096f16e0b869ed467af0a7e0
LAPTOP-KH3KN5AR\zhang <zhangmengli57@gmail.com>
2018-01-23 15:31:59 +0800

    no message

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var insurance = angular.module('insurance', ['threecars', 'batchnocooperation', 'nocooperation', 'addnocooperation', 'confirmation', 'backinfo', 'backtotal', 'brandcfg', 'member', 'brand', 'shop4sorder', 'shop4snum', 'updateFix', 'auditDetail', 'audit', 'third', 'auth', 'datastatistics', 'paymentdetail', 'batchinspector', 'payment', 'inspector', 'companyfleet', 'addshop4S', 'batchshop4S', 'shop4S', 'nar_location', 'disasterdriverorderlist', 'disastermap', 'driverordertotle', 'disasterorderlist', 'totleorder', 'batchaddorder', 'review', 'driverlocation', 'disasterdriver', 'disasterinspector', 'site', 'disasterdetail', 'disaster', 'createdisaster', 'addorder_nar', 'selectlocation', 'editorder', 'track', 'detail', 'team', 'ui.router', 'evaluation', 'adddriver', 'map', 'login', 'Road167', 'fixaddress', 'main', 'addorder', 'orderlist']);
+var insurance = angular.module('insurance', ['threecars', 'caijidetail', 'batchnocooperation', 'nocooperation', 'addnocooperation', 'confirmation', 'backinfo', 'backtotal', 'brandcfg', 'member', 'brand', 'shop4sorder', 'shop4snum', 'updateFix', 'auditDetail', 'audit', 'third', 'auth', 'datastatistics', 'paymentdetail', 'batchinspector', 'payment', 'inspector', 'companyfleet', 'addshop4S', 'batchshop4S', 'shop4S', 'nar_location', 'disasterdriverorderlist', 'disastermap', 'driverordertotle', 'disasterorderlist', 'totleorder', 'batchaddorder', 'review', 'driverlocation', 'disasterdriver', 'disasterinspector', 'site', 'disasterdetail', 'disaster', 'createdisaster', 'addorder_nar', 'selectlocation', 'editorder', 'track', 'detail', 'team', 'ui.router', 'evaluation', 'adddriver', 'map', 'login', 'Road167', 'fixaddress', 'main', 'addorder', 'orderlist']);
 var t;
 insurance.config(function ($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.when('', '/login');
@@ -215,6 +215,10 @@ insurance.config(function ($stateProvider, $urlRouterProvider) {
         .state('main.detail', {
             url: '/detail',
             templateUrl: 'view/detail/detail.html'
+        })
+        .state('main.caijidetail', {
+            url: '/caijidetail',
+            templateUrl: 'view/detail/caijidetail.html'
         })
         .state('main.payment.detail', {
             url: '/detail',
--- js/front/detail/caijidetail.js
+++ js/front/detail/caijidetail.js
@@ -1,15 +1,40 @@
 var caijidetail = angular.module('caijidetail', ['Road167']);
 caijidetail.controller('caijidetailCtrl', ['$scope', 'APIService', function ($scope, APIService) {
     $scope.initData = function () {
-
+        $scope.init();
+    }
+    $scope.init = function () {
+        $scope.url = sessionStorage.getItem('threecars')
+        if ($scope.url === 'threecars') {
+            $scope.title = '三者车采集详情'
+        } else {
+            $scope.title = '采集详情'
+        }
+    }
+    $scope.isWeicaijiNull = function (weicaiji, text) {
+        return weicaiji == '' ? text : '、' + text
     }
     $scope.detail = function () {
         APIService.get_order_detail().then(function (res) {
             if (res.data.http_status == 200) {
                 closeloading();
                 $scope.detail = res.data;
-                if (res.data.verifyAddressItems != null) {
-                    $scope.fix_change_list = res.data.verifyAddressItems.items.reverse();
+                $scope.weicaiji = '';
+                $scope.detail.licenseInfo = res.data;
+                if ($scope.detail.licenseInfo.idCard == null) {
+                    $scope.weicaiji = $scope.isWeicaijiNull($scope.weicaiji, '身份证');
+                }
+                if ($scope.detail.licenseInfo.drivingLicense == null) {
+                    $scope.weicaiji = $scope.weicaiji + $scope.isWeicaijiNull($scope.weicaiji, '行驶证');
+                }
+                if ($scope.detail.licenseInfo.driverLicense == null) {
+                    $scope.weicaiji = $scope.weicaiji + $scope.isWeicaijiNull($scope.weicaiji, '驾驶证');
+                }
+                if ($scope.detail.licenseInfo.insurancePolicy == null) {
+                    $scope.weicaiji = $scope.weicaiji + $scope.isWeicaijiNull($scope.weicaiji, '保单');
+                }
+                if ($scope.weicaiji.split('、').length == 4) {
+                    $scope.weicaiji = '无采集证照'
                 }
 
             } else {
